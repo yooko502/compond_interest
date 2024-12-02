8fb683d3bc3755057d2c65af8656c89d2533410d
yangxy <yang.xy0101@gmail.com>
2024-12-02 21:52:49 +0900

    add: form

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,7 +4,7 @@ import { InvestmentTabs } from "./components/investmentTabs";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] justify-items-center min-h-screen gap-16">
-      <div>定投小工具</div>
+      <div>投資シミュレーション</div>
       <main className="flex flex-col row-start-2 max-w-5xl mx-auto w-full">
         <InvestmentTabs />
       </main>
