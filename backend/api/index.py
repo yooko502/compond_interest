adbfb3785880819bfb0bdf851b30c10aabaa9b74
yangxy <yang.xy0101@gmail.com>
2024-12-04 23:33:53 +0900

    update: api

--- backend/api/index.py
+++ backend/api/index.py
@@ -37,3 +37,40 @@ def get_final_balance():
 
     response = jsonify({"result": result})
     return response
+
+@app.route("/api/back_to_present_amount", methods=["POST"])
+def get_back_to_present_amount():
+    json_data = request.get_json()
+    year_return = float(json_data.get("year_return", 0)) / 100 # 年收益率
+    target_amount = float(json_data.get("target_amount", 0)) # 目标金额
+    reserve_periods = int(json_data.get("reserve_periods", 0)) # 投资期间
+    
+    calc = InvestmentCalculator(
+        y_return=year_return,  # 年收益率
+        horizon=reserve_periods,  # 投资年份
+        # m_investment=0,  # 每月投资1000元
+        # init_balance=0,  # 初始余额0元
+        method="geometric",  # 使用几何平均值计算月收益率
+        # increment=0,
+        # incre_period=0
+    )
+
+    # 目標金額を達成するには
+    target_amount = 1000000
+    back_to_present = calc.back_to_present("amount", target_amount)
+
+    # chart 用数据
+    data = calc.automatic_investment()
+    result = {
+        "final_balance": data.final_balance,
+        "total_principal": data.total_principal,
+        "total_return": data.total_return,
+        "monthly_data": json.loads(data.monthly_data.to_json(orient="records"))
+    }
+
+    responese = jsonify({
+        "chart_data": result,
+        "back_to_present": back_to_present
+    })
+
+    return responese
