f871609e0c842a660894dbbb2d997efc096d8217
yangxiaoye <sho-yo@dts.com>
2024-12-05 15:54:36 +0900

    api commonality

--- backend/api/index.py
+++ backend/api/index.py
@@ -7,6 +7,11 @@ from backend.core.investment_calculator import InvestmentCalculator
 app = Flask(__name__)
 CORS(app)
 
+PRESENT_METHOD = {
+  "amount": "amount",
+  "rate": "rate",
+  "horizon": "horizon"
+}
 @app.route("/api/final_balance", methods=["POST"])
 def get_final_balance():
     json_data = request.get_json()
@@ -38,26 +43,39 @@ def get_final_balance():
     response = jsonify({"result": result})
     return response
 
-@app.route("/api/back_to_present_amount", methods=["POST"])
-def get_back_to_present_amount():
+@app.route("/api/present_data", methods=["POST"])
+def get_back_to_present():
+    present_method = request.args.get("target")
     json_data = request.get_json()
     year_return = float(json_data.get("year_return", 0)) / 100 # 年收益率
-    target_amount = float(json_data.get("target_amount", 0)) # 目标金额
-    reserve_periods = int(json_data.get("reserve_periods", 0)) # 投资期间
+    monthly_reserve = float(json_data.get("monthly_reserve", 0)) # 每月投资额度
+    initial_investment = float(json_data.get("initial_investment", 0))
+    reserve_periods = int(json_data.get("reserve_periods", 0))
+    increment = float(json_data.get("increment", 0))
+    incre_period = int(json_data.get("incre_period", 0))
     
     calc = InvestmentCalculator(
-        y_return=year_return,  # 年收益率
-        horizon=reserve_periods,  # 投资年份
-        # m_investment=0,  # 每月投资1000元
-        # init_balance=0,  # 初始余额0元
+        y_return=year_return,  # 10% 年收益率
+        horizon=reserve_periods,  # 5年投资期
+        m_investment=monthly_reserve,  # 每月投资1000元
+        init_balance=initial_investment,  # 初始余额0元
         method="geometric",  # 使用几何平均值计算月收益率
-        # increment=0,
-        # incre_period=0
+        increment=increment,
+        incre_period=incre_period
     )
 
     # 目標金額を達成するには
     # target_amount = 1000000
-    back_to_present = calc.back_to_present("amount", target_amount)
+    if present_method == PRESENT_METHOD["amount"]:
+        back_to_present = calc.back_to_present(present_method, target_amount)
+    
+    if present_method == PRESENT_METHOD["rate"]:
+        # TODO: GET RATE DATA
+        pass
+    
+    if present_method == PRESENT_METHOD["horizon"]:
+        # TODO: GET HORIZON DATA
+        pass
 
     # chart 用数据
     data = calc.automatic_investment(m_investment=back_to_present)
