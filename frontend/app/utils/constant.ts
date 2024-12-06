df1e38e11a7eaf3bce690f848c44eb7e79a8baa4
yangxiaoye <sho-yo@dts.com>
2024-12-06 13:59:55 +0900

    add present common enum

--- frontend/app/utils/constant.ts
+++ frontend/app/utils/constant.ts
@@ -1,3 +1,9 @@
 export const apiBaseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
-export const presentMethodsCard = ["withdrawal", "monthlySavings"] 
+export const presentMethodsCard = ["amount", "rate", "horizon"] 
+
+export enum presentCommonType {
+  amount="rate",
+  rate="amount",
+  horizon="horizon"
+}
