86757d4a9799b71b3c90a1c426c7c2f4e4e6f400
yangxy <yang.xy0101@gmail.com>
2024-12-07 21:39:29 +0900

    fix tabs button css for mobile

--- app/components/investmentTabs.tsx
+++ app/components/investmentTabs.tsx
@@ -12,19 +12,19 @@ import { presentMethodsCard } from "../utils/constant"
 
 export function InvestmentTabs() {
   return (
-    <Tabs defaultValue="accumulation" >
-      <TabsList className="grid grid-cols-4 max-w-5xl mx-auto w-full">
-        <TabsTrigger value="accumulation">つみたて投資シミュレーション</TabsTrigger>
-        <TabsTrigger value="rate">利回りシミュレーション</TabsTrigger>
-        <TabsTrigger value="amount">毎月積立額シミュレーション</TabsTrigger>
-        <TabsTrigger value="horizon">積立期間シミュレーション</TabsTrigger>
+    <Tabs defaultValue="accumulation">
+      <TabsList className="grid grid-cols-1 mb-24 md:mb-4 md:grid-cols-4 gap-1 bg-transparent md:bg-muted md:gap-2">
+        <TabsTrigger value="accumulation" className="data-[state=active]:bg-muted md:data-[state=active]:bg-[#ffffff]">つみたて投資シミュレーション</TabsTrigger>
+        <TabsTrigger value="rate" className="data-[state=active]:bg-muted md:data-[state=active]:bg-[#ffffff]">利回りシミュレーション</TabsTrigger>
+        <TabsTrigger value="amount" className="data-[state=active]:bg-muted md:data-[state=active]:bg-[#ffffff]">毎月積立額シミュレーション</TabsTrigger>
+        <TabsTrigger value="horizon" className="data-[state=active]:bg-muted md:data-[state=active]:bg-[#ffffff]">積立期間シミュレーション</TabsTrigger>
       </TabsList>
-      <TabsContent value="accumulation" className="max-w-5xl mx-auto w-full">
+      <TabsContent value="accumulation" className="mt-4 px-2 md:px-4">
         <FixedInvestmentCard />
       </TabsContent>
       {presentMethodsCard.map((item: string) => {
         return(
-          <TabsContent key={item} value={item}>
+          <TabsContent key={item} value={item} className="mt-4 px-2 md:px-4">
             <PresentCommonCard key={item} type={item}/>
           </TabsContent>
         )})
